70384474278d3f8f4f8a0e88b6a579809b7ddd6a
ikl2kor <ikl2kor@bosch.com>
2025-01-31 10:41:22 +0530

    riscv's critical-section enabled

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@
 
 use panic_halt as _;
 use riscv_rt::entry;
-use riscv_semihosting::hprintln;
+use riscv_semihosting::{hprintln, debug};
 // use semihosting::println;
 
 #[entry]
 fn main() -> ! {
 
     hprintln!("Hello, world!");
-
+    debug::exit(debug::EXIT_FAILURE);
     loop {
         // Main loop
     }
